ed8089f5c9c1d67e56405d0a79e533a9752e8667
zhang96 <jerry.fengwei@gmail.com>
2017-02-13 17:20:58 +0000

    added uncss lib, added donate dummy page, fixed two broken twitter link in posts, added unexscaping xml feature during auto deployment.

--- autodep.sh
+++ autodep.sh
@@ -2,6 +2,17 @@ echo "Locating CMS Folder"
 cd ~/TechFusionFM
 echo "Generating Static Files"
 hexo deploy
+echo "Unescaping xmly"
+echo "--replacing replace <"
+sed -ie 's/\&amp\;lt\;/\</g' ~/TechFusionFM/public/podcast.xml
+echo "--replacing replace >"
+sed -ie 's/\&amp\;gt\;/\>/g' ~/TechFusionFM/public/podcast.xml
+echo "--replacing quotation"
+sed -ie 's/\&amp\;quot\;/\"/g' ~/TechFusionFM/public/podcast.xml
+echo "--replacing ' "
+sed -ie "s/\&amp\;apos\;/\\'/g" ~/TechFusionFM/public/podcast.xml
+
+
 echo "Removing Existing Static Files on Apache Server"
 rm -rf /var/www/html/*
 echo "Coping Static Files to Apache Server"
