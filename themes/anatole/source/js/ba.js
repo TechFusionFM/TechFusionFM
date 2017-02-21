353bdb8bfe6da6438e53db7816c064c179c4b5a4
zhang96 <jerry.fengwei@gmail.com>
2017-02-21 19:53:35 +0000

    added Baidu Analytics

---
+++ themes/anatole/source/js/ba.js
@@ -0,0 +1,17 @@
+var _hmt = _hmt || [];
+
+(function() {
+
+  var hm = document.createElement("script");
+
+  hm.src = "https://hm.baidu.com/hm.js?82cd3d3a276f22486603ef433972d993";
+
+  var s = document.getElementsByTagName("script")[0]; 
+
+  s.parentNode.insertBefore(hm, s);
+
+})();
+
+
+
+
