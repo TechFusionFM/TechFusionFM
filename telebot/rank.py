e2ea8d464f11d7a7697e7d0086c58292decae7e6
Fengwei Zhang <jerry.fengwei@gmail.com>
2017-05-24 15:46:45 -0400

    used new source from apple

--- telebot/rank.py
+++ telebot/rank.py
@@ -6,20 +6,27 @@ import os
 import time
 from bs4 import BeautifulSoup
 while True:
-    url = 'https://podcast.okihika.com/CN/1318'
+    url = 'https://itunes.apple.com/cn/rss/toppodcasts/limit=100/genre=1318/xml'
     file = wget.download(url, bar=None)
-    html = open('1318', 'r')
-    soup = BeautifulSoup(html, 'html.parser')
+    markup = open('xml', 'r')
+    soup = BeautifulSoup(markup, "xml")
 
 # print(soup.prettify())
-    cell = soup.find(href = "/CN/iid/1202658654/%E7%A7%91%E6%8A%80%E8%81%9A%E5%8F%98%20(TechFusion)")
-    td = cell.find_parents("td")[0]
-    string = td.span.prettify()
-    string = string.splitlines()[1]
-    string = re.sub("\D", "", string)
-    print string
-    r = requests.get("https://api.telegram.org/bot370638413:AAHrBMWdw1QzNP6LJ2HBZqfhbrislpOv_4g/sendMessage?text=TechFusion%20is%20now%20at%20%23"+ string +"%20place%20on%20iTunes%20Technology.&chat_id=-1001126950310")
+    cell = soup.find_all('im:name')
+    print cell;
+    # td = cell.find_parents("entry")[0];
+    indicator = True;
+    counter = 0;
+    for item in cell:
+        if item.contents[0].find('TechFusion') != -1:
+             print item.contents[0];
+             counter += 1;
+             print 'nahhhasdfsadfasdf'
+             break;
+        else: 
+            print item.contents[0];
+            counter += 1
+    r = requests.get("https://api.telegram.org/bot370638413:AAHrBMWdw1QzNP6LJ2HBZqfhbrislpOv_4g/sendMessage?text=TechFusion%20is%20now%20at%20%23"+ str(counter) +"%20place%20on%20iTunes%20Technology.&chat_id=-1001126950310")
     print(r.status_code, r.reason)
-    os.remove("1318")
+    os.remove("xml")
     time.sleep(600)
-
