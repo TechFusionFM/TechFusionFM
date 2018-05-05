c4317d3dae69efa1085a67bf66739b7a5162a64c
Fengwei Jerry Zhang <zhang96@users.noreply.github.com>
2018-05-05 01:42:01 -0400

    remove public folder since the content is now generated on the prod server

--- public/js/ha.js
+++
@@ -1,1 +0,0 @@
-(function(t,h,e,j,n,s){t.hj=t.hj||function(){(t.hj.q=t.hj.q||[]).push(arguments)};t._hjSettings={hjid:528654,hjsv:5};n=h.getElementsByTagName("head")[0];s=h.createElement("script");s.async=1;s.src=e+t._hjSettings.hjid+j+t._hjSettings.hjsv;n.appendChild(s)})(window,document,"//static.hotjar.com/c/hotjar-",".js?sv=");
